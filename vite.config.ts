113c66eb3cdefb21520cac2d2a9b12306d9a27e5
determaer <determaer2012@gmail.com>
2025-03-20 21:01:29 +0700

    fix: ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
+      entry: path.resolve(__dirname, 'src/index.ts'),
       name: "vuessages",
       fileName: (format) => `vuessages.${format}.js`,
     },
