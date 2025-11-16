a0417912198c1d68ad3a4ea32a8c68d5654a9cba
determaer <determaer2012@gmail.com>
2025-11-16 14:42:18 +0700

    fix: remove prop-depending line direction

--- src/helpers/calcLinePoints.ts
+++ src/helpers/calcLinePoints.ts
@@ -4,59 +4,53 @@ import { calcControlPoint } from "./calcControlPoint";
 const store = useParamStore();
 
 export const calcLinePoint = (objLabelIn: Label, objLabelOut: Label) => {
-  const radiusCorrection = store.discNum.value >= 50 ? 1 : 0;
-  const objLabelInAngles = store.angles.value.find(
-    (lAngle) =>
-      lAngle.labelId ===
-      store.labelsZero.value.findIndex((label) => objLabelIn.id == label.id),
+  const labelInIndex = store.labelsZero.value.findIndex(
+    (label) => objLabelIn.id == label.id,
   );
-  const objLabelOutAngles = store.angles.value.find(
-    (lAngle) =>
-      lAngle.labelId ===
-      store.labelsZero.value.findIndex((label) => objLabelOut.id == label.id),
+  const labelOutIndex = store.labelsZero.value.findIndex(
+    (label) => objLabelOut.id == label.id,
   );
-  let bezierCPangle1: number,
-    bezierCPangle2: number,
-    outAngle: number,
-    inAngle: number,
-    outRadius: number,
-    inRadius: number;
+  const inAngles = store.angles.value.find(
+    (lAngle) => lAngle.labelId === labelInIndex,
+  );
+  const outAngles = store.angles.value.find(
+    (lAngle) => lAngle.labelId === labelOutIndex,
+  );
+  if (inAngles == undefined || outAngles == undefined) return [];
 
-  if (objLabelIn.prop !== 0 || objLabelOut.prop !== 0) {
-    inRadius = store.radiuses.value.innerRadius;
-    outRadius = store.radiuses.value.linesBtwElementsRadius;
-    outAngle = objLabelOutAngles?.inAngle ?? 0;
-    inAngle = objLabelInAngles?.outAngle ?? 0;
-  } else {
-    outRadius = store.radiuses.value.innerRadius;
-    inRadius = store.radiuses.value.linesBtwElementsRadius;
-    outAngle = objLabelOutAngles?.outAngle ?? 0;
-    inAngle = objLabelInAngles?.inAngle ?? 0;
-  }
+  const discNumCorrection = store.discNum.value >= 50 ? 1 : 0;
 
-  let diff = Math.abs(outAngle - inAngle);
-  if (outAngle >= 300) outAngle = outAngle - 360;
-  if (inAngle >= 300) inAngle = inAngle - 360;
-  let t = 0;
-  if (diff < 180) {
-    t = (180 - diff) / 10;
-    diff = diff / 250;
-    bezierCPangle1 = outAngle + diff;
-    bezierCPangle2 = inAngle - diff;
-  } else {
-    t = (180 - Math.abs(diff - 360)) / 10;
-    diff = diff / 250;
-    bezierCPangle1 = outAngle - diff;
-    bezierCPangle2 = inAngle + diff;
-  }
+  const diff = Math.abs(outAngles.outAngle - inAngles.inAngle);
+
+  const outAngle =
+    outAngles.outAngle >= 300 ? outAngles.outAngle - 360 : outAngles.outAngle;
+
+  const inAngle =
+    inAngles.inAngle >= 300 ? inAngles.inAngle - 360 : inAngles.inAngle;
+
+  const bezierCPangle1 =
+    diff < 180 ? outAngle + diff / 250 : outAngle - diff / 250;
+
+  const bezierCPangle2 =
+    diff < 180 ? inAngle - diff / 250 : inAngle + diff / 250;
+
+  const angleCorrection =
+    diff < 180 ? (180 - diff) / 10 : (180 - Math.abs(diff - 360)) / 10;
 
   const radius =
-    10 * t * store.scaleMultiplier.value +
-    50 * radiusCorrection * store.scaleMultiplier.value;
+    (10 * angleCorrection + 50 * discNumCorrection) *
+    store.scaleMultiplier.value;
+
   const [bezierCPX1, bezierCPY1] = calcControlPoint(radius, bezierCPangle1);
   const [bezierCPX2, bezierCPY2] = calcControlPoint(radius, bezierCPangle2);
-  const [outX, outY] = calcControlPoint(outRadius, outAngle);
-  const [inX, inY] = calcControlPoint(inRadius, inAngle);
+  const [outX, outY] = calcControlPoint(
+    store.radiuses.value.innerRadius,
+    outAngle,
+  );
+  const [inX, inY] = calcControlPoint(
+    store.radiuses.value.linesBtwElementsRadius,
+    inAngle,
+  );
 
   return [outX, outY, bezierCPX1, bezierCPY1, bezierCPX2, bezierCPY2, inX, inY];
 };
